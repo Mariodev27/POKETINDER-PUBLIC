f854e25672ad41909f607f35de819807dcfcdb23
Mario <mariosantisteban23@gmail.com>
2023-07-01 14:17:25 -0500

    Lab14

--- app/src/main/java/com/santisteban/mario/poketinder/ui/view/SplashActivity.kt
+++ app/src/main/java/com/santisteban/mario/poketinder/ui/view/SplashActivity.kt
@@ -1,11 +1,16 @@
 package com.santisteban.mario.poketinder.ui.view
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.content.Intent
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.InstanceIdResult
 import com.santisteban.mario.poketinder.databinding.ActivitySplashBinding
 import com.santisteban.mario.poketinder.ui.viewmodel.SplashViewModel
 
@@ -15,6 +20,22 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener<InstanceIdResult?> { task ->
+                if(!task.isSuccessful){
+                    Log.w(
+                        TAG, "getInstanceId failed",
+                        task.exception
+                    )
+                    return@OnCompleteListener
+                }
+                //Get new Instance ID token
+                val token = task.result.token
+                Log.e(
+                    "FIREBASE TOKEN", "Token: $token"
+                )
+            })
 
         splashViewModel.getIsUnderMaintenance().observe(this){isUnderMaintenance ->
             if(isUnderMaintenance){
